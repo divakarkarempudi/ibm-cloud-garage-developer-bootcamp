ca9191521ea75222933dc568cdf91b0a9895b1b4
Divakar Karempudi <divakar.karempudi@aa.com>
2017-08-11 14:55:48 -0500

    palindrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,5 +1,15 @@
-describe('the palindrome canary spec', () => {
+describe.only('the palindrome canary spec', () => {
   it('shows the test infrastructure works', () => {
     true.should.be.true();
+  });
+
+  describe('palindrome', () => {
+    it('be true for mom');
+    it('false for dude');
+    it('true for a');
+    it('true for mom dad mom');
+
+    it('false for ""');
+    it('false for "       "');
   });
 });
