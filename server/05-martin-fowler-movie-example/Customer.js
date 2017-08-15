c99a9f058f3e6a882cbd2fc0b2aff36c7498adfe
Divakar Karempudi <divakar.karempudi@aa.com>
2017-08-15 14:36:13 -0500

    Refactoring

--- server/05-martin-fowler-movie-example/Customer.js
+++ server/05-martin-fowler-movie-example/Customer.js
@@ -1,70 +1,43 @@
-import {Movie} from './Movie';
-
 let Customer = (name) => {
   let rentals = [];
 
-  function amountFor(aRental) {
-    let result = 0;
-    //noinspection Eslint
-    switch (aRental.movie.priceCode) {
-      case Movie.REGULAR:
+  function getTotalCharge() {
+    let totalAmount = 0;
 
-        result += 2;
-        if (aRental.daysRented > 2) {
-          result += (aRental.daysRented - 2) * 1.5;
-        }
-        break;
-
-      case Movie.NEW_RELEASE:
-
-        result += aRental.daysRented * 3;
-        break;
-
-      case Movie.CHILDRENS:
-
-        result += 1.5;
-        if (aRental.daysRented > 3) {
-          result += (aRental.daysRented - 3) * 1.5;
-        }
-
-        break;
-      default:
-
+    for (let i = 0; i < rentals.length; i++) {
+      let each = rentals[i];
+      totalAmount += each.getCharge();
     }
-    return result;
+    return totalAmount;
   }
+
+  function getTotalFrequentRenterPoints() {
+    let frequentRenterPoints = 0;
+
+     for (let i = 0; i < rentals.length; i++) {
+       let each = rentals[i];
+       frequentRenterPoints += each.getFrequentRenterPoints();
+     }
+     return frequentRenterPoints;
+   }
 
   return {
     get name() { return name; },
     addRental(rental) { rentals.push(rental); },
 
     statement() {
-      let totalAmount = 0;
-      let frequentRenterPoints = 0;
       let result = 'Rental Record for ' + name + '\n';
 
       for (let i = 0; i < rentals.length; i++) {
-        let thisAmount = 0;
         let each = rentals[i];
-        thisAmount = amountFor(each);
-
-        // add frequent renter points
-        frequentRenterPoints++;
-
-        // add bonus for a two day new release each
-        if ((each.movie.priceCode === Movie.NEW_RELEASE) &&
-            each.daysRented > 1) frequentRenterPoints++;
 
         //show figures for this each
-        result += '\t' + each.movie.title + '\t' + thisAmount.toString(10) + '\n';
-
-        totalAmount += thisAmount;
+        result += '\t' + each.movie.title + '\t' + each.getCharge().toString(10) + '\n';
       }
 
       //add footer lines
-      result += 'Amount owed is ' + totalAmount.toString(10) + '\n';
-      result += 'You earned ' + String(frequentRenterPoints) +
-          ' frequent renter points';
+      result += 'Amount owed is ' + getTotalCharge().toString(10) + '\n';
+      result += 'You earned ' + String(getTotalFrequentRenterPoints()) + ' frequent renter points';
 
       return result;
     }
--- server/05-martin-fowler-movie-example/Rental.js
+++ server/05-martin-fowler-movie-example/Rental.js
@@ -1,6 +1,47 @@
+import {Movie} from './Movie';
+
 let Rental = (movie, daysRented) => {
 
+  function getCharge() {
+    let result = 0;
+    //noinspection Eslint
+    switch (movie.priceCode) {
+      case Movie.REGULAR:
+
+        result += 2;
+        if (daysRented > 2) {
+          result += (daysRented - 2) * 1.5;
+        }
+        break;
+
+      case Movie.NEW_RELEASE:
+
+        result += daysRented * 3;
+        break;
+
+      case Movie.CHILDRENS:
+
+        result += 1.5;
+        if (daysRented > 3) {
+          result += (daysRented - 3) * 1.5;
+        }
+
+        break;
+      default:
+
+    }
+    return result;
+  }
+
+  function getFrequentRenterPoints() {
+    // add bonus for a two day new release each
+    if ((movie.priceCode === Movie.NEW_RELEASE) && daysRented > 1) return 2;
+    return 1;
+  }
+
   return {
+    getCharge,
+    getFrequentRenterPoints,
     get movie() { return movie; },
     get daysRented() { return daysRented; }
   };
